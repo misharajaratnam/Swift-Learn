74aa341a9d71865cf1b41ebc698d27bb34018123
Misha Rajaratnam <mishar@mishas-macbook-pro.local>
2019-06-08 00:15:49 -0700

    First Swift Lesson

--- iOS Swift Learn 1/ViewController.swift
+++ iOS Swift Learn 1/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var buttonCount = 0
+    
+   
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func ButtonTapped(_ sender: Any) {
+        buttonCount = buttonCount + 1
+         print (buttonCount)
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.red
+            myLabel.text = "Gabriel is playing"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
 
